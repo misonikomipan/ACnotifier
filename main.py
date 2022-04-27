069e8a2c800b03c25abbf166542c1f7c52d611af
misonikomipan <yusakum1311@gmail.com>
2022-04-27 21:52:14 +0900

    make a function that post to discord

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import json
 import requests
-from webhook import webhook_url
+# from data import webhook_url, users, payload
 
 
 def get_problems_json():
@@ -21,6 +21,18 @@ def get_problems_json():
     return problems_json_data
 
 
+def post_to_discord(msg):
+    global payload
+    global webhook_url
+    payload["content"] = msg
+    try:
+        r = requests.post(webhook_url, data=payload)
+        r.raise_for_status() # check the status code
+        print(f"status: {r.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"error: {e}")
+
+
 def DC_converter(num): #difficulty to color converter
     colors = [':white_heart:', ':brown_heart:', ':green_heart:', ':sweat_drops:' ':blue_heart:' ,':yellow_heart:', ':orange_heart:', 'heart']
     key = min(len(colors) - 1, max(0, num // 400))
@@ -28,12 +40,15 @@ def DC_converter(num): #difficulty to color converter
 
 
 def check_submission(usr, time):
+    print(f"--{usr}の提出をチェック--")
     global problems_json_data
     global webhook_url
+    global payload
     user_url = f"https://kenkoooo.com/atcoder/atcoder-api/results?user={usr}&from_second={time}"
     l_AC_time = time
+    pad = "-" * len(usr)
 
-    # get one's submission data
+    # get usr's submission data
     try:
         responce = requests.get(user_url)
         responce.raise_for_status()  # check the status code
@@ -41,13 +56,12 @@ def check_submission(usr, time):
         res = responce.json()
     except requests.exceptions.RequestException as e:
         print(f"error: {e}")
-        return -1
 
-    # check one's submission
+    # check usr's submission
     for result in res:
-        if result['epoch_second'] > time and result['result'] == "AC":     # find a new AC
-            l_AC_time = max(l_AC_time, result['epoch_second'])    # update latest AC time
-            ac_time_not_epoch = datetime.datetime.fromtimestamp(result['epoch_second'])
+        if result['epoch_second'] > time and result['result'] == "AC":      # find a new AC
+            l_AC_time = max(l_AC_time, result['epoch_second'])              # update latest AC time
+            ac_datetime = datetime.datetime.fromtimestamp(result['epoch_second'])
             problem_id = result['problem_id']
             submit_url = f"https://atcoder.jp/contests/{result['contest_id']}/submissions/{result['id']}"
             try:
@@ -57,50 +71,41 @@ def check_submission(usr, time):
             except KeyError:
                 diff = "???"
                 color = "???"
+            # notify
+            message = f"**{usr}**が*{problem_id}*をACしました！({ac_datetime})\rDiff: {color}\r{submit_url}"
+            print(message)
+            post_to_discord(message)
 
-            # post to discord channel
-            payload = {
-                    "username": "ACnotifier",
-                    "avatar_url": "https://www.ad-c.or.jp/images/logo_l_new.png",
-                    "content": f"**{usr}**が*{problem_id}*をACしました！({ac_time_not_epoch})\rDiff: {color}\r{submit_url}"
-                    }
-            try:
-                r = requests.post(webhook_url, data=payload)
-                r.raise_for_status() # check the status code
-                print(f"status: {r.status_code}")
-            except requests.exceptions.RequestException as e:
-                print(f"error: {e}")
-                return -1
-            print(f"{usr}が{problem_id}をAC！({result['epoch_second']})")
-
+    if l_AC_time == time: print("No sub")
+    print(f"{pad}--------------------\n")
     return l_AC_time
 
 
 if __name__ == '__main__':
-    users = ['take000', 'misonikomipan', 'hasubo', 'aanyachan', 'shionchan1230']
+    webhook_url = "hogehoge"
+    users = ['misonikomipan']
+    payload = {
+                    "username": "ACnotifier",
+                    "avatar_url": "https://www.ad-c.or.jp/images/logo_l_new.png",
+                    "content": "test"
+                    }
+    message = "チェック開始!"
+    print(message)
+    post_to_discord(message)
     latest_AC_time = int(time.time()) - 300 # before 10 minutes (initial value) 
     try:
         while 1: # main loop
             before = latest_AC_time
             problems_json_data = get_problems_json()
-            time.sleep(1)
+            time.sleep(5)
             for user_name in users:
-                print(f"--{user_name}の提出をチェック--")
                 ac_time = check_submission(user_name, before)
-                if ac_time == before: print("No sub")
                 latest_AC_time = max(latest_AC_time, ac_time)
-                pad = "-" * len(user_name)
-                print(f"{pad}--------------------\n")
-                time.sleep(1)
+                time.sleep(2)
         
     except KeyboardInterrupt:
         # Ctrl-C
-        print('KEY BOARD INTERRUPT!!!')
-        payload = {
-                        "username": "ACnotifier",
-                        "avatar_url": "https://www.ad-c.or.jp/images/logo_l_new.png",
-                        "content": f"**動作を停止したよ！**"
-                        }
-        r = requests.post(webhook_url, data=payload)
+        message = '**動作を停止したよ！**(KEYBOARD INTERRUPT)'
+        print(message)
+        post_to_discord(message)
         exit()
-
